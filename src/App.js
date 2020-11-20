c9ff9ccaeb3b59f7aa271618b755ebc4f4cb8abf
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-11-20 16:07:06 +0530

    useState() with arrays

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ErrorExample from "./tutorial/1-useState/setup/1-error-example";
+import UseStateArray from "./tutorial/1-useState/setup/3-useState-array";
 import UseStateObject from "./tutorial/1-useState/setup/4-useState-object";
 import UseStateCounter from "./tutorial/1-useState/setup/5-useState-counter";
 
 function App() {
   return (
     <div className="container">
-      <ErrorExample />
+      {/* 
+        <ErrorExample />
       <UseStateObject />
       <UseStateCounter />
+      */}
+      <UseStateArray />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,53 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  console.log(data);
+
+  const handleClick = (id) => {
+    const updatedPersons = people.filter((person) => person.id !== id);
+    setPeople(updatedPersons);
+  };
+
+  const handleClearAll = () => {
+    setPeople([]);
+  };
+  const personsJSX =
+    people &&
+    people.map((person) => {
+      return (
+        <div
+          key={person.id}
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "1rem",
+            border: "1px solid #ccc",
+            width: "500px",
+            margin: "0 auto",
+          }}
+        >
+          <span>{person.name}</span>
+          <span
+            onClick={() => handleClick(person.id)}
+            style={{ cursor: "pointer" }}
+          >
+            remove
+          </span>
+        </div>
+      );
+    });
+  return (
+    <main>
+      <h1>People in the Department :</h1>
+      {personsJSX}
+      <button style={{ margin: "0 auto" }} onClick={handleClearAll}>
+        {" "}
+        clear all
+      </button>
+    </main>
+  );
 };
 
 export default UseStateArray;
