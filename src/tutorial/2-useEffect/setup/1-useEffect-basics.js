7752b51e61ad282af15761ad727bf9240b288c29
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-11-20 19:28:18 +0530

    Dependency List as 2nd argument to useEffect() and many ways it runs

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -5,11 +5,27 @@ import React, { useState, useEffect } from "react";
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
 
+  /* ---
   useEffect(() => {
     console.log("useEffect run !!!");
-    document.title = `Title Updated : ${value}`;
+    document.title = value > 0 ? `Title Updated : ${value}` : ` Default Title`;
   });
+  --- */
 
+  // Dependency List - passing list of arrays as second argument
+
+  // ComponentDidMount
+  useEffect(() => {
+    console.log("Initial Render | useEffect() run");
+  }, []);
+
+  // ComponentDidUpdate
+  /* ---
+  useEffect(() => {
+    console.log("useEffect() run");
+    document.title = value > 0 ? `Title Updated : ${value}` : ` Default Title`;
+  }, [value]);
+  ---*/
   const handleClick = () => {
     setValue((prevValue) => {
       return prevValue + 1;
