47ffb7a2835206aa8ceb5315eb221eedad135753
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-11-20 14:51:14 +0530

    Creating counter and updating state with hooks using function as argument is setCounter()

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ErrorExample from "./tutorial/1-useState/setup/1-error-example";
 import UseStateObject from "./tutorial/1-useState/setup/4-useState-object";
+import UseStateCounter from "./tutorial/1-useState/setup/5-useState-counter";
 
 function App() {
   return (
     <div className="container">
       <ErrorExample />
       <UseStateObject />
+      <UseStateCounter />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [counter, setCounter] = useState(0);
+
+  const handleComplexCounter = (e) => {
+    console.log("Complex Counter");
+    setTimeout(() => {
+      setCounter((prevState) => prevState + 1);
+    }, 2000);
+  };
+  return (
+    <section style={{ border: "1px solid #ccc" }}>
+      <article>
+        <h4 style={{ margin: "1rem 0" }}>Regular Counter</h4>
+        <p>{counter}</p>
+        <button onClick={(e) => setCounter(counter + 1)}>increase</button>
+        <button onClick={(e) => setCounter(0)}>reset</button>
+        <button onClick={(e) => setCounter(counter - 1)}>decrease</button>
+      </article>
+
+      <article>
+        <p>{counter}</p>
+        <h4 style={{ margin: "1rem 0" }}>Complex Counter</h4>
+        <button onClick={handleComplexCounter}>increase</button>
+      </article>
+    </section>
+  );
 };
 
 export default UseStateCounter;
