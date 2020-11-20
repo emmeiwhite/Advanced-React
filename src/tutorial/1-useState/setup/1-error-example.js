e205012648d68737b153af0fb617ab731de78e71
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-11-20 12:10:51 +0530

    useState basic example

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [title, setTitle] = useState("Change Title");
+  const changeTitle = () => {
+    if (title === "Change Title") {
+      setTitle("Rock the World");
+    } else {
+      setTitle("Change Title");
+    }
+  };
+  return (
+    <React.Fragment>
+      <p>{title}</p>
+      <button onClick={changeTitle}>Change Title</button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
