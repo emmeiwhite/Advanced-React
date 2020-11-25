7cdc206b9ac98ef5400b9468567cff95632a3d18
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-11-25 15:15:11 +0530

    Controlled Inputs to sync value with input

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./controlled-inputs.css";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +7,97 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [persons, setPersons] = useState([]);
+  const [isFormValid, setIsFormValid] = useState(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name && email && age) {
+      console.log({
+        name,
+        email,
+        age,
+      });
+      const person = {
+        name,
+        email,
+        age,
+      };
+      setIsFormValid(true);
+      setPersons([...persons, person]);
+      setAge("");
+      setName("");
+      setEmail("");
+    } else {
+      setIsFormValid(false);
+      console.log("Form cannot be submitted without values");
+    }
+  };
+
+  return (
+    <main className="form-wrapper">
+      <form>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            id="name"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            id="email"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="age">Age</label>
+          <input
+            type="text"
+            name="age"
+            onChange={(e) => setAge(e.target.value)}
+            value={age}
+            id="age"
+          />
+        </div>
+
+        {!isFormValid && (
+          <h3 style={{ color: "red" }}>Fill all the form fields </h3>
+        )}
+        <div>
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="submit-button"
+          >
+            Submit Details
+          </button>
+        </div>
+      </form>
+
+      <section className="persons-details">
+        <h3>PERSONS</h3>
+        {persons.length > 0 &&
+          persons.map((person) => (
+            <div className="person-card">
+              <span>{person.name}</span>
+              <span>{person.age}</span>
+              <span>{person.email}</span>
+            </div>
+          ))}
+      </section>
+    </main>
+  );
 };
 
 export default ControlledInputs;
