a9c7174dc57455575996c1668d6aa79182dc96a2
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-12-11 11:58:54 +0530

    Setting defaultProps

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import defaultImage from "./../../../assets/default-image.jpeg";
 
 /* --- We'll work with PropTypes ---*/
 const Product = ({ name, image, price }) => {
   return (
     <article className="product">
       <p>Name: {name}</p>
-      <p>Image: {image.url}</p>
-      <p>Price: {price}</p>
+      <img src={image.url} alt={name} />
+      <p>Price:$ {price}</p>
     </article>
   );
 };
@@ -16,5 +17,12 @@ Product.propTypes = {
   name: PropTypes.string.isRequired,
   image: PropTypes.object.isRequired,
   price: PropTypes.number.isRequired,
+};
+
+// Default Props | Whenever any prop for the Product Component is missing we will use our default value for smooth execution of the program
+Product.defaultProps = {
+  name: "Random Name",
+  price: 3.99,
+  image: defaultImage,
 };
 export default Product;
