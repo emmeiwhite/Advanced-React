84858a4d5743dfc8d2581e64fbc7cf29f46de109
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-12-11 12:05:52 +0530

    Setting default values using || operator

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -4,11 +4,13 @@ import defaultImage from "./../../../assets/default-image.jpeg";
 
 /* --- We'll work with PropTypes ---*/
 const Product = ({ name, image, price }) => {
+  /* --- Awesome Techniques to deal with undefined object --- */
+  const url = image && image.url;
   return (
     <article className="product">
-      <p>Name: {name}</p>
-      <img src={image.url} alt={name} />
-      <p>Price:$ {price}</p>
+      <p>{name}</p>
+      <img src={url || defaultImage} alt={name} />
+      <p>${price || 3.99}</p>
     </article>
   );
 };
@@ -19,10 +21,12 @@ Product.propTypes = {
   price: PropTypes.number.isRequired,
 };
 
-// Default Props | Whenever any prop for the Product Component is missing we will use our default value for smooth execution of the program
+// Default Props | Whenever any prop for the Product Component is missing we will use our default value for smooth execution of the program. Alternate way to handle default Props is using || operator and && operator trickily
+/* ---
 Product.defaultProps = {
   name: "Random Name",
   price: 3.99,
   image: defaultImage,
 };
+---*/
 export default Product;
