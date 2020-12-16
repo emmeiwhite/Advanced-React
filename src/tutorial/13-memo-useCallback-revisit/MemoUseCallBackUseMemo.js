b03b70be9deb2c9e514f1a52ba4aeb15385e36ba
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-12-16 15:20:32 +0530

    useCallback(): addProduct fxn will only re-create when value of products changes

--- src/tutorial/13-memo-useCallback-revisit/MemoUseCallBackUseMemo.js
+++ src/tutorial/13-memo-useCallback-revisit/MemoUseCallBackUseMemo.js
@@ -1,27 +1,81 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import "./MemoUseCallBackUseMemo.css";
+
+const myProducts = [
+  { id: 1, name: "laptop" },
+  { id: 2, name: "desktop" },
+  { id: 3, name: "cup" },
+];
 
 export default function MemoUseCallBackUseMemo() {
   const [count, setCount] = useState(0);
+  const [products, setProducts] = useState(myProducts);
+
+  const addProduct = useCallback(
+    (productName) => {
+      const product = {
+        id: new Date().getTime().toString(),
+        name: productName,
+      };
+
+      setProducts([...products, product]);
+    },
+    [products]
+  );
+  /*--- with useCallback the callback function will only update when the products change---*/
+
   return (
     <div>
       <h4>Count:{count}</h4>
       <button onClick={() => setCount(count + 1)}>Increase Count</button>
 
-      <ChildComponent />
+      <ChildComponent products={products} addProduct={addProduct} />
     </div>
   );
 }
+/*--- In the above setup: Whenever the state variable count increases, 
+    MemoUseCallBackUseMemo component get's re-rendered.Now, since ChildComponent is used inside this component, it also gets re-rendered even when no prop change happens in the Component. products passed stays in their original state. The solution is React.memo()
+---*/
 
-const ChildComponent = () => {
+const ChildComponent = React.memo(({ products, addProduct }) => {
+  const [product, setProduct] = useState("");
   useEffect(() => {
     console.count("ChildComponent");
   });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (product) {
+      addProduct(product);
+      setProduct("");
+    }
+  };
   return (
-    <div style={{ marginTop: "2rem" }}>
+    <div
+      style={{ marginTop: "2rem", border: "1px solid #000", padding: "1rem" }}
+    >
       <h4>Child Component</h4>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, quas.
       </p>
+
+      <div className="products-wrapper">
+        {products.length > 0 &&
+          products.map((product) => {
+            return <div key={product.id}>{product.name}</div>;
+          })}
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="product"
+          value={product}
+          onChange={(e) => setProduct(e.target.value)}
+          required
+        />
+        <button type="submit">Add Product</button>
+      </form>
     </div>
   );
-};
+});
